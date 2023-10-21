5d2a1d12001ed7b03238c88efd5409a2dceaea99
serhiogoal <serhii.kryvyi1807@gmail.com>
2023-10-22 00:37:21 +0100

    Message components storage was added.

---
+++ script.js
@@ -0,0 +1,5 @@
+const messages = {
+    teams: ['Arsenal', 'ManchesterCity', 'Chelsea', 'Real Madrid', 'Barcelona', 'Atletico', 'Manchester United', 'Shakhtar', 'Bayern', 'Borissia', 'Milan', 'Inter', 'PSG'],
+    verbs: [' are not ', ' are ', ' might be ', ' can be ', ' are 100% '],
+    nouns: ['losers!', 'winners!', 'unlucky.', 'lucky!', 'the best!', 'succesful.', 'disgusting...']
+}
