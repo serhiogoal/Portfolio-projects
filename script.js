8dcf6e960f9b62ec1f5cf2a1cd260a60354a742c
serhiogoal <serhii.kryvyi1807@gmail.com>
2023-10-22 00:52:04 +0100

    Program puts random message into console now.

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const messages = {
     verbs: [' are not ', ' are ', ' might be ', ' can be ', ' are 100% '],
     nouns: ['losers!', 'winners!', 'unlucky.', 'lucky!', 'the best!', 'succesful.', 'disgusting...']
 }
+
+let message;
+const createTheMessage = () => {
+    const firstWord = messages.teams[Math.floor(Math.random() * messages.teams.length)];
+    const secondWord = messages.verbs[Math.floor(Math.random() * messages.verbs.length)];
+    const thirdWord = messages.nouns[Math.floor(Math.random() * messages.nouns.length)];
+    message = firstWord + secondWord + thirdWord;
+    console.log(message);
+}
+
+createTheMessage();
